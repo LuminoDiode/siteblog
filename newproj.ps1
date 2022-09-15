210aa52d0658f20d8300f1f3a1585e66bc983aa0
Rodion R <65biso2@gmail.com>
2022-09-16 00:58:07 +0700

    minor post-init addings

--- newproj.ps1
+++ newproj.ps1
@@ -4,8 +4,8 @@ dotnet new sln --name siteblog;
 dotnet sln add backend/backend.csproj;
 dotnet new gitignore;
 npx create-react-app client --template typescript;
-New-Item image_indexator_backend/dockerfile;
-New-Item image_indexator_client/dockerfile;
+New-Item backend/dockerfile;
+New-Item client/dockerfile;
 New-Item docker-compose.yml;
 New-Item docker-compose.override.yml;
 New-Item .dockerignore;
@@ -33,5 +33,13 @@ Set-Content .dockerignore '**/.classpath
 **/secrets.dev.yaml
 **/values.dev.yaml
 LICENSE
-README.md'
+README.md
+**/*secrets.json
+'
 git init;
+Add-Content .gitignore '
+docker-compose.override.yml
+wwwroot/user_added/
+*secrets.json'
+
+Read-Host -Prompt "Press Enter to exit"
