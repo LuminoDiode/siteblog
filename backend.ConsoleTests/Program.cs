c01c5192d247b15615512f8ec4f3938b59d8dde8
Rodion R <65biso2@gmail.com>
2022-11-01 23:40:18 +0300

    DraftService release condidate ready.

--- backend.ConsoleTests/Program.cs
+++ backend.ConsoleTests/Program.cs
@@ -1,4 +1,7 @@
-﻿using backend.Services;
+﻿using backend.Models.Database;
+using backend.Repository;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Moq;
 using System.IdentityModel.Tokens.Jwt;
@@ -12,61 +15,63 @@ namespace backend.ConsoleTests
 	{
 		static void Main(string[] args)
 		{
-			Mock<JwtServiceSettingsProvider> jwtSettingsProvider = new(null);
-			jwtSettingsProvider.SetupGet(x => x.signingKey).Returns("OVERRIDE_MEOVERRIDE_ME");
-			jwtSettingsProvider.SetupGet(x => x.tokenLifespanDays).Returns(360);
+			//Mock<JwtServiceSettingsProvider> jwtSettingsProvider = new(null);
+			//jwtSettingsProvider.SetupGet(x => x.signingKey).Returns("OVERRIDE_MEOVERRIDE_ME");
+			//jwtSettingsProvider.SetupGet(x => x.tokenLifespanDays).Returns(360);
 
 
-			Mock<EmailConfirmationServiceSettingsProvider> emailConfirmSettingsProvider = new(null);
-			emailConfirmSettingsProvider.SetupGet(x => x.ownDomain).Returns(@"mysite.com/");
-			emailConfirmSettingsProvider.SetupGet(x => x.linkLifespanDays).Returns(1);
-			emailConfirmSettingsProvider.SetupGet(x => x.urlPathBeforeToken).Returns(@"/api/emailConfirmation/");
+			//Mock<EmailConfirmationServiceSettingsProvider> emailConfirmSettingsProvider = new(null);
+			//emailConfirmSettingsProvider.SetupGet(x => x.ownDomain).Returns(@"mysite.com/");
+			//emailConfirmSettingsProvider.SetupGet(x => x.linkLifespanDays).Returns(1);
+			//emailConfirmSettingsProvider.SetupGet(x => x.urlPathBeforeToken).Returns(@"/api/emailConfirmation/");
 
-			Mock<SettingsProviderService> _settingsProviderService = new(null);
+			//Mock<SettingsProviderService> _settingsProviderService = new(null);
 
 
-			_settingsProviderService.SetupGet(x => x.JwtServiceSettings).Returns(jwtSettingsProvider.Object);
-			_settingsProviderService.SetupGet(x => x.EmailConfirmationServiceSettings).Returns(emailConfirmSettingsProvider.Object);
+			//_settingsProviderService.SetupGet(x => x.JwtServiceSettings).Returns(jwtSettingsProvider.Object);
+			//_settingsProviderService.SetupGet(x => x.EmailConfirmationServiceSettings).Returns(emailConfirmSettingsProvider.Object);
 
-			var service = new EmailConfirmationService(_settingsProviderService.Object, new JwtService(_settingsProviderService.Object),null,null);
+			//var service = new EmailConfirmationService(_settingsProviderService.Object, new JwtService(_settingsProviderService.Object),null,null);
 
-			var link = service.CreateLinkForEmail("testemail@gmail.com");
-			var parsed =  service.GetEmailFromLinkIfValid(link);
-			//Console.ReadKey();
+			//var link = service.CreateLinkForEmail("testemail@gmail.com");
+			//var parsed =  service.GetEmailFromLinkIfValid(link);
+			////Console.ReadKey();
 
-			var _tokenHandler = new JwtSecurityTokenHandler();
-			var token = _tokenHandler.WriteToken(_tokenHandler.CreateToken(new SecurityTokenDescriptor
-			{
-				Subject = new ClaimsIdentity(new Claim[] { new Claim("username", "bidlo") }),
-				Expires = DateTime.UtcNow.AddSeconds(1),
-				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")), SecurityAlgorithms.HmacSha512Signature)
-			}));
-			Console.WriteLine("waitin");
-			Task.Delay(1000).Wait();
-			Console.WriteLine("validating");
-			var result = _tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
-			{
-				ValidateIssuer = false,
-				ValidateAudience = false,
-				ValidateLifetime = true,
-				ValidateIssuerSigningKey = true,
-				IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")),
-				ClockSkew = TimeSpan.Zero
-			}).Result;
-			for(int i = 0; i < 1000; i++)
-			{
-				 result = _tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
-				{
-					ValidateIssuer = false,
-					ValidateAudience = false,
-					ValidateLifetime = true,
-					ValidateIssuerSigningKey = true,
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")),
-					 ClockSkew = TimeSpan.Zero
-				 }).Result;
-				Console.WriteLine(result.IsValid);
-				Task.Delay(1000).Wait();
-			}
+			//var _tokenHandler = new JwtSecurityTokenHandler();
+			//var token = _tokenHandler.WriteToken(_tokenHandler.CreateToken(new SecurityTokenDescriptor
+			//{
+			//	Subject = new ClaimsIdentity(new Claim[] { new Claim("username", "bidlo") }),
+			//	Expires = DateTime.UtcNow.AddSeconds(1),
+			//	SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")), SecurityAlgorithms.HmacSha512Signature)
+			//}));
+			//Console.WriteLine("waitin");
+			//Task.Delay(1000).Wait();
+			//Console.WriteLine("validating");
+			//var result = _tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
+			//{
+			//	ValidateIssuer = false,
+			//	ValidateAudience = false,
+			//	ValidateLifetime = true,
+			//	ValidateIssuerSigningKey = true,
+			//	IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")),
+			//	ClockSkew = TimeSpan.Zero
+			//}).Result;
+			//for(int i = 0; i < 1000; i++)
+			//{
+			//	 result = _tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
+			//	{
+			//		ValidateIssuer = false,
+			//		ValidateAudience = false,
+			//		ValidateLifetime = true,
+			//		ValidateIssuerSigningKey = true,
+			//		IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("OVERRIDEMEOVERRIDEME")),
+			//		 ClockSkew = TimeSpan.Zero
+			//	 }).Result;
+			//	Console.WriteLine(result.IsValid);
+			//	Task.Delay(1000).Wait();
+			//}
+
+			DraftService<PostDraft,BlogContext> t = new(new BlogContext(), null, x => x.PostDrafts, x => true, x => DateTime.UtcNow);
 			Console.ReadKey();
 		}
 	}
--- backend/Services/DraftService.cs
+++ backend/Services/DraftService.cs
@@ -3,58 +3,101 @@ using backend.Models.Database;
 using backend.Models.Runtime;
 using backend.Repository;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading;
 
 namespace backend.Services
 {
 
-	public class DraftService<T> : BackgroundService where T: class
+	public /* abstract */ class DraftService<TEntity, TContext> 
+		: BackgroundService, IDisposable, IAsyncDisposable
+		where TEntity : class
+		where TContext : DbContext
 	{
 		protected class StoredDraft
 		{
 			public DateTime LastUpdatedDateTimeUtc;
-			public T Entity;
+			public TEntity Entity;
 
-			public StoredDraft(DateTime lastUpdatedDateTime, T entity)
+			public StoredDraft(DateTime lastUpdatedDateTime, TEntity entity)
 			{
 				this.LastUpdatedDateTimeUtc = lastUpdatedDateTime;
 				this.Entity = entity;
 			}
 		}
 
-		protected readonly BlogContext _blogContext;
-		protected virtual SettingsProviderService _settingsProvider { get; init; }
+		protected readonly TContext? _blogContext;
+		protected readonly SettingsProviderService _settingsProvider;
 		protected virtual DraftServiceSettings _settings => _settingsProvider.DraftServiceSettings;
 		protected readonly List<StoredDraft> RamStored;
-		protected readonly Func<DbContext, DbSet<T>> _dbSetSelector;
-		protected readonly Func<T, bool> _entitySelector;
+		protected readonly Func<TContext, DbSet<TEntity>> _dbSetSelector;
+		protected readonly Func<TEntity, bool> _entitySelector;
+		protected readonly Func<TEntity, DateTime> _entityLastUpdateSelector;
 
+		protected Func<StoredDraft, bool> inRamOutdatedPredicate;
+		protected Func<TEntity, bool> inDbOutdatedPredicate;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DraftService"/> class.
+		/// </summary>
+		/// <param name="dbContext">
+		/// Nullable db context to be used for saving to db. 
+		/// In case of null only ram will be used for storing drafts.
+		/// </param>
+		/// <param name="settingsProvider">
+		/// SettingsProvider for the service.
+		/// </param>
+		/// <param name="dbSetSelector">
+		/// Delegate for getting needed dbSet from the TContext.
+		/// </param>
+		/// <param name="entitySelector">
+		/// Delegate for getting needed entity from the collections.
+		/// This delegate must be translateable for Queryable in case of
+		/// using TContext (if it is not null).
+		/// </param>
+		/// <param name="entityLastUpdateFieldSelector">
+		/// Delegate for getting last update time from the stored entities.
+		/// </param>
 		public DraftService(
-			BlogContext blogContext, 
-			SettingsProviderService settingsProvider, 
-			Func<DbContext,DbSet<T>> dbSetSelector,
-			Func<T, bool> EntitySelector)
+			TContext? dbContext,
+			SettingsProviderService settingsProvider,
+			Func<TContext, DbSet<TEntity>> dbSetSelector,
+			Func<TEntity, bool> entitySelector,
+			Func<TEntity, DateTime> entityLastUpdateFieldSelector)
 		{
-			_blogContext = blogContext;
+			_blogContext = dbContext;
 			_settingsProvider = settingsProvider;
 			_dbSetSelector = dbSetSelector;
-			_entitySelector = EntitySelector;
+			_entitySelector = entitySelector;
+			_entityLastUpdateSelector = entityLastUpdateFieldSelector;
 
 			RamStored = new(_settings.maxEntitiesStoredInRam);
 
-			throw new NotImplementedException();
+			inRamOutdatedPredicate = (x =>
+				x.LastUpdatedDateTimeUtc.AddMinutes(_settings.maxTimeEntityStoredInRamMinutes)
+				< DateTime.UtcNow);
+			inDbOutdatedPredicate = (x =>
+				_entityLastUpdateSelector(x).AddMinutes(_settings.maxTimeEntityStoredInDbMinutes)
+				< DateTime.UtcNow);
 		}
 
-		public void PushDraft(T entity)
+		public void PushDraft(TEntity entity)
 		{
 			this.RamStored.Add(new(DateTime.UtcNow, entity));
 		}
 
-		public T? GetDraft(Func<T, bool> EntitySelector)
+		public TEntity? GetDraftFromRam(Func<TEntity, bool> predicate)
 		{
-			var found = RamStored.FirstOrDefault(x => EntitySelector(x.Entity));
-			return found is null ? default(T) : found.Entity;
+			var found = RamStored.FirstOrDefault(x => predicate(x.Entity));
+			return found is null ? default(TEntity) : found.Entity;
 		}
+
+		public Task<TEntity?> GetDraftFromDbAsync(Func<TEntity, bool> predicate)
+		{
+			if (_blogContext is null) return Task.FromResult(default(TEntity));
+			return _dbSetSelector(_blogContext).FirstOrDefaultAsync(x => predicate(x));
+		}
+
 
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
@@ -62,34 +105,83 @@ namespace backend.Services
 			{
 				await Task.Delay((int)(_settings.updateStoredEntitiesIntervalMinutes * 60 * 100));
 
-				await DeleteOutdatedFromDbAsync();
-				await SaveOutdatedFromRamToDbAsync();
+				if (_blogContext is null)
+				{
+					DeleteOutdatedFromRam();
+				}
+				else
+				{
+					await DeleteOutdatedFromDbAsync(_blogContext);
+					await SaveOutdatedFromRamToDbAsync(_blogContext);
+				}
 			}
 		}
 
-		protected async Task SaveOutdatedFromRamToDbAsync()
+		protected void DeleteOutdatedFromRam()
 		{
-			var toBeSaved = this.RamStored.Where(x =>
-				x.LastUpdatedDateTimeUtc.AddMinutes(_settings.maxTimeEntityStoredInRamMinutes)
-				< DateTime.UtcNow);
+			RamStored.RemoveAll(x => inRamOutdatedPredicate(x));
+		}
 
+		protected async Task SaveOutdatedFromRamToDbAsync(TContext ctx)
+		{
+			// discovering all the entities to save
+			var toBeSaved = this.RamStored.Where(x => inRamOutdatedPredicate(x));
+
+			// saving all the discovered to db
 			foreach (var ramStored in toBeSaved)
 			{
-				var foundInDb = await _dbSetSelector(_blogContext).AsTracking().SingleOrDefaultAsync(x=> _entitySelector(x));
-				if (foundInDb is null) await _dbSetSelector(_blogContext).AddAsync(ramStored.Entity);
-				else _blogContext.Entry(foundInDb).CurrentValues.SetValues(ramStored.Entity);
+				var foundInDb = await _dbSetSelector(ctx).AsTracking().SingleOrDefaultAsync(x => _entitySelector(x));
+
+				// if such id not found - add new row
+				if (foundInDb is null) await _dbSetSelector(ctx).AddAsync(ramStored.Entity);
+				// if it is - update in the db
+				else ctx.Entry(foundInDb).CurrentValues.SetValues(ramStored.Entity);
 
 				this.RamStored.Remove(ramStored);
 			}
 
-			await _blogContext.SaveChangesAsync();
+			// save to the db
+			await ctx.SaveChangesAsync();
+
+			// detach all the entities
+			foreach (var ramStored in toBeSaved)
+			{
+				ctx.Entry(ramStored).State = EntityState.Detached;
+			}
 		}
 
-		protected async Task DeleteOutdatedFromDbAsync()
+		protected async Task DeleteOutdatedFromDbAsync(TContext ctx)
 		{
-			throw new NotImplementedException();
+			var toDelete = _dbSetSelector(ctx).Where(
+				x => (DateTime.UtcNow - _entityLastUpdateSelector(x))
+				> TimeSpan.FromMinutes(_settings.maxTimeEntityStoredInDbMinutes));
+
+			_dbSetSelector(ctx).RemoveRange(toDelete);
+
+			await ctx.SaveChangesAsync();
 		}
 
+		// saving all the entities to db
+		public override void Dispose()
+		{
+			if (_blogContext is not null)
+			{
+				inRamOutdatedPredicate = x => true;
+				SaveOutdatedFromRamToDbAsync(_blogContext).Wait();
+			}
+
+			base.Dispose();
+		}
+		public async ValueTask DisposeAsync()
+		{
+			if (_blogContext is not null)
+			{
+				inRamOutdatedPredicate = x => true;
+				await SaveOutdatedFromRamToDbAsync(_blogContext);
+			}
+
+			base.Dispose();
+		}
 	}
 }
 
